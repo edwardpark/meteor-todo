d4d7baa7b40861ce2d5e665ec118d5251863ad96
Edward Park <parkes.edward@gmail.com>
2015-06-26 14:58:21 -0400

    added header and listing functionality

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -1,13 +1,31 @@
-// simple-todos.js
 if (Meteor.isClient) {
   // This code only runs on the client
+
+  Tasks = new Mongo.Collection("tasks");
+
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
+    tasks: function(){
+      return Tasks.find({});
+
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function(event){
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+
+      });
+      event.target.text.value = "";
+      //return false; //prevent defautl form submit.    
+    } //end of new task form submit .new-task
+
   });
 
 
-}
+
+
+} //end of .isClient
